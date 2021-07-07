e944394bc9e370fdc57d8287046682cc6632edc0
guotin <eero.kauhanen@helsinki.fi>
2021-07-07 14:57:28 +0300

    test artifact downloading

---
+++ download_artifacts.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+import shutil
+import glob
+import os
+
+
+ARTIFACTS_URL = sys.argv[1]
+GITHUB_TOKEN = sys.argv[2]
+
+response = requests.get(ARTIFACTS_URL)
+data = response.json()
+i = 0
+for artifact in data["artifacts"]:
+    download_url = artifact["archive_download_url"]
+    print("Artifact download url:", download_url)
+    artifact_response = requests.get(
+        download_url,
+        headers={"Authorization": f"token {GITHUB_TOKEN}"},
+    )
+    print("Artifact download status code:", artifact_response.status_code)
+    if artifact_response.status_code == 200:
+        filepath = f"report{i}.zip"
+        with open(filepath, "wb") as f:
+            f.write(artifact_response.content)
+        i += 1
+        print(f"wrote file {filepath}")
+
+i = 0
+for archive_path in glob.iglob("./*.zip"):
+    shutil.unpack_archive(archive_path, "./")
+    os.rename("./test_results.xml", f"./report{i}.xml")
+    i += 1
+
+print("unpacked all artifacts")
